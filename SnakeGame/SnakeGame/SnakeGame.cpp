5497fb49db61a68483ae3606b90b02b423b47db5
OmarRefaee <orefaee10@gmail.com>
2025-12-07 03:40:07 +0200

    Added Functions To Classes

--- SnakeGame/SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame/SnakeGame.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
+#include <thread> // For std::this_thread::sleep_for
+#include <chrono> // For std::chrono::seconds, milliseconds, etc.
+#include <conio.h>
+#include <string>
 using namespace std;
 
 // ----------------------------
@@ -9,55 +13,225 @@ struct Position {
     int x, y;
 };
 
-vector<Position> snake;
-Position food;
-int score;
-int dirX, dirY;
-const int WIDTH = 20;
+const int WIDTH = 50;
 const int HEIGHT = 15;
 
-// ----------------------------
-// 1) TAREK – Input + Movement
-// ----------------------------
-void handleInput() {
-    // TODO: read input W/A/S/D and update dirX, dirY
-}
+// OOP Classes
 
-void moveSnake() {
-    // TODO: move head + move body + handle growth
-}
+// AHMED - Input + Movement
+class Snake {
 
-// ----------------------------
-// 2) ABDO ELSAYED – Food System
-// ----------------------------
-void spawnFood() {
-    // TODO: randomly place food not inside snake
-}
+private:
+    vector<Position> snake;
+    int dirX, dirY;
+    enum  enDirection { W = 1, S = 2, D = 3, A = 4 };
+public:
+    Position head = { 5, 10 };
+    vector<Position> body;
+    enDirection Dir;
+    // ----------------------------
+    // 1) AHMED – Input + Movement
+    // ----------------------------
+    void handleInput() {
+        if (_kbhit())
+        {
+            //Using toupper to Convert any Characters to Capital
+            //Using _getch() 
+            char  c = _getch();
+            c = toupper(c);
+            switch (c)
+            {
+            case 'W':
+                Dir = enDirection::W;
+                break;
+            case 'S':
+                Dir = enDirection::S;
+                break;
+            case 'A':
+                Dir = enDirection::A;
+                break;
 
-bool eatenFood() {
-    // TODO: return true if snake head touches food
-    return false;
-}
+            case 'D':
+                Dir = enDirection::D;
+                break;
 
-// ----------------------------
-// 3) OMAR – Game Manager
-// ----------------------------
-bool isDead() {
-    // TODO: return true if snake hits wall or itself
-    return false;
-}
+            default:
+                break;
+            }
+        }
+    }
 
-void draw() {
-    // TODO: print map, snake, food, score
-}
+    void moveSnake() {
+        switch (Dir)
+        {
+        case W:
+            head.y--;
+            break;
+        case S:
+            head.y++;
+            break;
+        case A:
+            head.x--;
+            break;
+        case D:
+            head.x++;
+            break;
+        }
+    }
+};
 
-void resetGame() {
-    // TODO: reset snake, direction, score, spawn food
-}
+class Board {
+private:
+    Position food;
+
+public:
+    vector<Position> food1;
+
+    void spawnFood(vector<Position> body) {
+
+
+        bool valid = false;
+
+        while (!valid) {
+            food.x = rand() % (WIDTH - 2) + 1;
+            food.y = rand() % (HEIGHT - 2) + 1;
+            valid = true;
+            for (int i = 0; i < body.size(); i++) {
+                if (food.x == body[i].x && food.y == body[i].y)
+                    valid = false;
+                break;
+            }
+        }
+        food1.push_back(food);
+    }
+
+
+
+    bool eatenFood(Position head) {
+
+
+        for (int i = 0; i < food1.size(); i++) {
+            if (head.x == food1[i].x && head.y == food1[i].y)
+            {
+                food1.erase(food1.begin() + i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+// OMAR - Game Manager
+class Game {
+private:
+
+public:
+    int score = 0;
+    // ----------------------------
+    // 3) OMAR – Game Manager
+    // ----------------------------
+    bool isDead(Position tester, vector<Position> testerBody) {
+        // TODO: return true if snake hits wall or itself
+        if (tester.x == 0 || tester.y == 0 || tester.x == WIDTH - 1 || tester.y == HEIGHT - 1)
+        {
+            cout << "GAME OVER" << endl;
+            return true;
+        }
+        else
+        {
+            for (int i = 0; i < testerBody.size(); i++)
+            {
+                if (tester.x == testerBody[i].x && tester.y == testerBody[i].y)
+                {
+                    cout << "GAME OVER" << endl;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void draw(Position tester, vector<Position> testerBody, vector<Position> testerFood) {
+        // TODO: print map, snake, food, score
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+
+                if (j == 0)
+                    cout << "*";
+                else if (j == WIDTH - 1)
+                    cout << "*";
+                else if (i == 0 || i == HEIGHT - 1)
+                    cout << "*";
+                else if (tester.x == j && tester.y == i)
+                    cout << "O";
+                else {
+                    bool tf = true;
+                    for (int k = 0; k < testerBody.size(); k++)
+                    {
+                        if (testerBody[k].x == j && testerBody[k].y == i)
+                        {
+                            cout << "-";
+                            tf = false;
+                        }
+                    }
+                    for (int l = 0; l < testerFood.size(); l++)
+                    {
+                        if (testerFood[l].x == j && testerFood[l].y == i)
+                        {
+                            cout << "@";
+                            tf = false;
+                        }
+                    }
+                    if (tf)
+                        cout << " ";
+                }
+
+            }
+            cout << endl;
+        }
+        cout << "SCORE: " << score << endl;
+    }
+
+    void resetGame(Game game1, Snake snake1) {
+        // TODO: reset snake, direction, score, spawn food
+        Snake snake2;
+        snake1 = snake2;
+
+        Game game2;
+        game1 = game2;
+    }
+};
 
 int main() {
-    cout << "Hello Test" << endl;
+    cout << "SNAKE GAME" << endl;
     // TODO: initialize game
     // TODO: loop: handleInput → moveSnake → eatenFood → isDead → draw
+    Game game1;
+    Snake snake1;
+    Board board1;
+
+    board1.spawnFood(snake1.body);
+    while (true)
+    {
+        //std::cout << "\033[2J\033[1;1H";
+        system("cls");
+        snake1.handleInput();
+        snake1.moveSnake();
+        if (board1.eatenFood(snake1.head))
+        {
+            board1.spawnFood(snake1.body);
+            game1.score++;
+        }
+        if (game1.isDead(snake1.head, snake1.body))
+        {
+            break;
+        }
+        game1.draw(snake1.head, snake1.body, board1.food1);
+        this_thread::sleep_for(chrono::milliseconds(200));
+    }
     return 0;
 }
